aa434147519403706e014a86f0649aed8c79b920
William-95 <verga.william.95@gmail.com>
2024-01-17 18:01:19 +0100

    Corso

--- src/esercizi/DistributoreBenzina.java
+++ src/esercizi/DistributoreBenzina.java
@@ -4,11 +4,30 @@ public class DistributoreBenzina {
     private double deposito=0;        //benzina disponibile nel distributore
     private double euroPerLitro;    //prezzo benzina
 
-    public DistributoreBenzina(double unPrezzoPerLitro){}
-
-
+    public DistributoreBenzina(double unPrezzoPerLitro){
+        this.euroPerLitro=unPrezzoPerLitro;
+    }
+    public void rifornisci(double unaQuantita){
+        deposito+=unaQuantita;
+        System.out.println("Sono stati riforniti "+unaQuantita+" l.");
+    }
+    public void vendi(double euro, Car unaAutomobile){              //da finire
+        double benzinaAcquistata= euroPerLitro/euro;
+        System.out.println("Aquistati l: "+benzinaAcquistata);
+    }
+    public void aggiorna(double unPrezzoPerLitro){   //aggiorna il prezzp della benzina
+        this.euroPerLitro=unPrezzoPerLitro;
+        System.out.println("il prezzo della benzina è di $ "+unPrezzoPerLitro+"/l.");
+    }
+    public void getDeposito(){
+        System.out.println("Deposito: "+deposito);
+    }
     public static void main(String[] args) {
-DistributoreBenzina dist= new DistributoreBenzina(14);
-        System.out.println(dist.deposito);
+        DistributoreBenzina dist= new DistributoreBenzina(1.89);
+        dist.rifornisci(100);
+        dist.getDeposito();
+        dist.aggiorna(10);
+        System.out.println("Prezzo carburante: "+dist.euroPerLitro);
+        dist.vendi(10,new Car(14));
     }
 }
